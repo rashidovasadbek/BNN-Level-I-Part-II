35e4c1b9e7d2f7f4970e55a863041ac328c41524
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-09-20 12:48:21 +0500

    N41-HT2

---
+++ N41-HT2/EmailSenderService.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Mail;
+using System.Net;
+using Microsoft.VisualBasic;
+
+namespace N41_HT2;
+
+public class EmailSenderService
+{
+    public SmtpClient SmtpClientInstance { get; init; }
+    private readonly object _lock = new();
+    public EmailSenderService()
+    {
+        lock (_lock)
+        {
+            SmtpClientInstance = new SmtpClient("smtp.gmail.com", 587);
+            SmtpClientInstance.Credentials = new NetworkCredential("sultonbek.rakhimov.recovery@gmail.com", "szabguksrhwsbtie");
+            SmtpClientInstance.EnableSsl = true;
+        }
+    }
+
+    public Task<bool> SendEmailAsync(string receiverEmailAddress, string subject, string body)
+    {
+    return Task.Run(async () =>
+    {
+        var result = true;
+        try
+        {
+            var smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.Credentials = new NetworkCredential("sultonbek.rakhimov.recovery@gmail.com", "szabguksrhwsbtie");
+            smtp.EnableSsl = true;
+
+            var mail = new MailMessage("rashidovasadbek264@gmail.com", receiverEmailAddress);
+            mail.Subject = subject;
+            mail.Body = body;
+
+                await smtp.SendMailAsync(mail);
+            
+        }
+        catch (Exception e)
+        {
+            result = false;
+        }
+
+        return result;
+    });
+    }
+}
---
+++ N41-HT2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using N41_HT2;
+
+var emailService = new EmailSenderService();
+var emailAddresses = new List<string>
+{
+    "sultonbek.rakhimov@gmail.com",
+    "jasurabdulxaev@gmail.com",
+    "abdura52.uz@gmail.com",
+    "toshmurodovj13@gmail.com",
+    "kbunyod3011@gmail.com",
+    "azamamonov555@gmail.com",
+    "abdurahmonsadriddinov0412@gmail.com",
+    ".com",
+    "rashidovasadbek264@gmail.com"
+};
+
+var sendRegistrationEmailTasks = emailAddresses.Select(async emailAddress =>
+{
+    Console.WriteLine($"Current thread Id inside Select - {Environment.CurrentManagedThreadId}");
+    var result = await emailService.SendEmailAsync(emailAddress, "Confirm you account", "Enter to this link");
+    Console.WriteLine($"Current thread Id inside Select - {Environment.CurrentManagedThreadId}");
+    Console.WriteLine($"{emailAddress} ga email jo'natish resultati - {result}");
+    return result;
+});
