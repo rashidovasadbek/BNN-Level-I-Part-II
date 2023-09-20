b19b1c7fa7b2bd83a47997b9fd9ecc13d3f041a8
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-09-20 12:47:57 +0500

    N41-HT1

---
+++ N41-HT1/Program.cs
@@ -0,0 +1,15 @@
+﻿using N41_HT1;
+
+var queue = new ThreadSafeQueueWithLock<int>();
+queue.Enqueue(1);
+queue.Enqueue(2);
+queue.Enqueue(3);
+queue.Enqueue(4);
+
+int item;
+if(queue.Dequeue(out item))
+{
+    Console.WriteLine(item);
+}
+
+
---
+++ N41-HT1/ThreadSafeQueueWithLock.cs
@@ -0,0 +1,35 @@
+﻿namespace N41_HT1;
+
+public class ThreadSafeQueueWithLock<T>
+{
+    private Queue<T> queue;
+    private readonly object _lock =new ();
+    public ThreadSafeQueueWithLock()
+    {
+        queue = new Queue<T>();
+    }
+
+    public void Enqueue(T item)
+    {
+        lock (_lock)
+        {
+            queue.Enqueue(item);
+        }
+    }
+    public bool Dequeue(out T item)
+    {
+        lock (_lock)
+        {
+            if (queue.Count > 0) 
+            {
+              item =  queue.Dequeue();   
+                return true;
+            }
+            else
+            {
+                item = default;
+                return false;
+            }
+        }
+    }
+}
