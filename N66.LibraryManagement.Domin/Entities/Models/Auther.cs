6bf75fca270730a9b8ea1c8922404d5a7e3e6c48
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-10-31 14:40:12 +0500

    Create Clean Architectore and Added Models

---
+++ N66.LibraryManagement.Domin/Entities/Models/Auther.cs
@@ -0,0 +1,10 @@
+﻿namespace N66.LibraryManagement.Domin.Entities.Models;
+
+public class Auther
+{
+    public Guid Id { get; set; } 
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+}
---
+++ N66.LibraryManagement.Domin/Entities/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace N66.LibraryManagement.Domin.Entities.Models;
+
+public class Book
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public Guid AutherId { get; set; } 
+}
