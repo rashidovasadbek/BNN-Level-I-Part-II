521ef7b39de7df393358d4faef3e7045f4209ede
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-10-31 14:39:30 +0500

    MyFirstConsoleAppWithDB

---
+++ MyFirstConsoleAppWithDB/Persistence/DataContext/AppDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using N66_C.Domain.Entities;
+
+namespace MyFirstConsoleAppWithDB.Persistence.DataContext;
+
+public class AppDBContext : DbContext
+{
+    public DbSet<Book> Books => Set<Book>();
+    public DbSet<Author> Authors => Set<Author>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    { 
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=MyFirstEfCoreApp;Username=postgres;Password=725264");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Book>().HasOne(book => book.Author).WithMany();
+
+    }
+}
---
+++ MyFirstConsoleAppWithDB/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyFirstConsoleAppWithDB.Persistence.DataContext;
+using N66_C.Domain.Entities;
+using System;
+using System.Text.Json;
+
+var services = new ServiceCollection();
+
+services.AddDbContext<AppDBContext>();
+
+var serviceProvider = services.BuildServiceProvider();
+var appDbContext = serviceProvider.GetRequiredService<AppDBContext>();
+
+if (!appDbContext.Authors.Any())
+{
+    appDbContext.Authors.AddRange(new Author
+    {
+        FirstName = "John", 
+        LastName = "Doe"
+    },
+        new Author
+        {
+            FirstName = "Jonibek",
+            LastName = "Doniyorov"
+        });
+
+    var changedRowsCount = await appDbContext.SaveChangesAsync();
+}
+
+if (appDbContext.Authors.Any() && !appDbContext.Books.Any())
+{
+    appDbContext.Books.AddRange(new Book
+    {
+        Title = "Asp.NET Core in Action 2018",
+        Description = "Programming",
+        AuthorId = appDbContext.Authors.First().Id
+    },
+        new Book
+        {
+            Title = "Asp.NET Core in Action 2021",
+            Description = "Programming",
+            AuthorId = appDbContext.Authors.Skip(1).First().Id
+        });
+
+    var changedRowsCount = await appDbContext.SaveChangesAsync();
+}
+
+var allBooks = await appDbContext.Books
+    .Include(book => book.Author)
+    .ToListAsync();
+Console.WriteLine(JsonSerializer.Serialize(allBooks));
